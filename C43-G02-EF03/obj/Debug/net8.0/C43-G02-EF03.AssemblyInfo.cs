1adcf32c8344bbc8b5a99b8406181e243e0a6824
Badr Ahmed Abdel_hady <123932289+BadrAhmed140@users.noreply.github.com>
2025-02-17 15:52:44 +0200

    first

--- C43-G02-EF03/obj/Debug/net8.0/C43-G02-EF03.AssemblyInfo.cs
+++ C43-G02-EF03/obj/Debug/net8.0/C43-G02-EF03.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C43-G02-EF03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eb11d565abd6400f37d67271910387862305dc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("C43-G02-EF03")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C43-G02-EF03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
